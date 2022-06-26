fe4bc00dad370b456f0806967c0a5c89aa534f77
Yanel0 <108235122+Yanel0@users.noreply.github.com>
2022-06-26 15:54:24 -0500

    Add files via upload

---
+++ 3 numeros mayor intermedio menor.py
@@ -0,0 +1,6 @@
+n1 = float(input("ingrese el primer numero: "))
+n2 = float(input("ingrese el segundo numero: "))
+n3 = float(input("ingrese el tercer numero: "))
+
+if n1 < n2 >n3:
+    print("el numero mayor es el primer numero")
