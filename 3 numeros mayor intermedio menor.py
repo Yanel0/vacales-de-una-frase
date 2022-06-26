e2254a5bd83fb2053f3e14c5398a3b1d27969fd1
Yanel0 <108235122+Yanel0@users.noreply.github.com>
2022-06-26 15:55:24 -0500

    Delete 3 numeros mayor intermedio menor.py

--- 3 numeros mayor intermedio menor.py
+++
@@ -1,6 +0,0 @@
-n1 = float(input("ingrese el primer numero: "))
-n2 = float(input("ingrese el segundo numero: "))
-n3 = float(input("ingrese el tercer numero: "))
-
-if n1 < n2 >n3:
-    print("el numero mayor es el primer numero")
